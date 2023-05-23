af4bd508c340490b83a275df3c9f1826084d2c2d
dashalalalala <dchernikaa@gmail.com>
2023-05-22 19:04:36 -0700

    added the module shell and some placeholder info

---
+++ client/src/pages/learn/Learn.js
@@ -0,0 +1,14 @@
+import Form from "../../components/form/Form";
+import StoreFront from "../../components/storeFront/StoreFront";
+import "./Learn.scss";
+
+function Learn() {
+	return (
+		<div className="learn-page">
+			<Form />
+			<StoreFront />
+		</div>
+	);
+}
+
+export default Learn;
