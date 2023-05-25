ee2fa18e13ac82ed27cdff36bb7adc3f7942b6bf
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-05-25 15:20:42 -0400

    reverted module back to normal

--- client/src/pages/Module/Module.jsx
+++ client/src/pages/Module/Module.jsx
@@ -1,35 +1,33 @@
-import React, {useEffect, useState} from "react";
-import { useNavigate, useParams } from 'react-router-dom';
-import Guide from '../../components/Guide/Guide';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Guide from "../../components/Guide/Guide";
 import { BASE_API_URL } from "../../helper";
-import axios from 'axios';
-
-
+import axios from "axios";
 
 export default function Module() {
-
   // const { id } = useParams();
   // const modules = ['Cash Flow 101', 'Choosing Team 101', 'Customer Retention 101', 'Product Development 101' ];
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-  const [questionData, setQuestionData] =useState(null);
+  const [questionData, setQuestionData] = useState(null);
 
+  useEffect(() => {
+    axios
+      .get(`${BASE_API_URL}`)
+      .then((res) => {
+        setQuestionData(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-	useEffect(() => {
-		axios.get(`${BASE_API_URL}`).then((res)=> {
-			setQuestionData(res.data);
-      setLoading(false);
-		})
-		.catch((error) => {
-            console.error(error);
-          });
-	},[]);
-
-	console.log(questionData);
+  console.log(questionData);
 
   if (loading) {
     return (
-      <div >
+      <div>
         <h1>Loading.....</h1>
       </div>
     );
@@ -39,15 +37,16 @@ export default function Module() {
 
   return (
     <>
-    <div>
-      <h1 className='title'>Select Your Module</h1>
-       {questionData.map((data, index) => (
-        <div onClick={() => navigate(`${data.id}`)}>
-        <p id={data.id} className="text">{data.name}</p>
-        </div>
-       ))}
+      <div>
+        <h1 className="title">Select Your Module</h1>
+        {questionData.map((data, index) => (
+          <div onClick={() => navigate(`${data.id}`)}>
+            <p id={data.id} className="text">
+              {data.name}
+            </p>
+          </div>
+        ))}
       </div>
-      
     </>
-  )
+  );
 }
